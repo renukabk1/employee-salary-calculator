35c38e01d74d86746c205b1a1ef0569a9d224d3f
Ubuntu <ubuntu@ip-172-31-16-59.eu-north-1.compute.internal>
2026-07-11 16:21:03 +0000

    Initial cmake project

---
+++ build/CMakeFiles/EmployeeSalaryCalculator.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EmployeeSalaryCalculator.dir/link.d"
+  "CMakeFiles/EmployeeSalaryCalculator.dir/src/Employee.cpp.o"
+  "CMakeFiles/EmployeeSalaryCalculator.dir/src/Employee.cpp.o.d"
+  "CMakeFiles/EmployeeSalaryCalculator.dir/src/SalaryCalculator.cpp.o"
+  "CMakeFiles/EmployeeSalaryCalculator.dir/src/SalaryCalculator.cpp.o.d"
+  "CMakeFiles/EmployeeSalaryCalculator.dir/src/main.cpp.o"
+  "CMakeFiles/EmployeeSalaryCalculator.dir/src/main.cpp.o.d"
+  "EmployeeSalaryCalculator"
+  "EmployeeSalaryCalculator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EmployeeSalaryCalculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/EmployeeSalaryCalculator.dir/src/SalaryCalculator.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/EmployeeSalaryCalculator.dir/src/SalaryCalculator.cpp.o: \
+ /home/ubuntu/employee-salary-calculator/src/SalaryCalculator.cpp \
+ /usr/include/stdc-predef.h \
+ /home/ubuntu/employee-salary-calculator/include/SalaryCalculator.h
